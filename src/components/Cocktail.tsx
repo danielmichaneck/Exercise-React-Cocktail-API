b83e1d61561b7f9f69adf9a14204f89f5a4933af
danielmichaneck <daniel.michaneck@gmail.com>
2024-08-12 17:09:43 +0200

    Working on interpreting the data.

--- src/components/Cocktail.tsx
+++ src/components/Cocktail.tsx
@@ -1,12 +1,20 @@
 import { ReactElement } from "react";
+import { ICocktail } from "../interfaces";
 
 interface CocktailProps {
     text: string;
 }
 
 export function Cocktail({text}: CocktailProps): ReactElement {
+    let cocktail: ICocktail;
+
+
     fetchCocktail().then((drinks) => {
-        console.log("Fetched drinks: " + drinks);
+        const cocktails: ICocktail[] = drinks.map((drink: any) => (
+            
+        ))
+        console.log("Fetched drinks: " + JSON.stringify(drinks));
+
         return <p>{drinks}</p>
     })
     setTimeout(() => {
@@ -17,13 +25,33 @@ export function Cocktail({text}: CocktailProps): ReactElement {
     return <></>
 }
 
+function formatCocktail(data: any): ICocktail {
+    let ingredients: string[15];
+    for (let i = 0; i < 15; i++) {
+        const index = "strIngredient" + (i + 1).toString();
+        if (eval("data." + index + " !== null")) {
+            ingredients.push(eval("data." + index));
+        }
+    }
+
+    const cocktail = {
+        alcoholic: data.strAlcoholic === "Alcoholic",
+        category: data.strCategory,
+        glass: data.strGlass,
+        iba: data.strIBA,
+        id: data.idDrink,
+        image: data.strImageSource,
+        ingredients: 
+    }
+}
+
 async function fetchCocktail() {
     try {
         const response = await fetch("https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita");
         console.log("Response: " + response)
-        const drinks = await response.text();
-        console.log("Drinks: " + drinks)
-        return drinks;
+        const result = await response.json();
+        console.log("Drinks: " + result)
+        return result.drinks;
     }
     catch(error) {
         console.log(error);
---
+++ src/interfaces.ts
@@ -0,0 +1,12 @@
+export interface ICocktail {
+    alcoholic: boolean,
+    category: string,
+    glass: string,
+    iba: string,
+    id: string,
+    image: string;
+    ingredients: string[],
+    instructions: string[],
+    name: string,
+    thumbnail: string,
+}
